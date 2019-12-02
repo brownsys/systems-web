b2a6a4198e5139ab79b53931ed1d3f43d6851f2c
Nicholas DeMarinis <ndemarinis@wpi.edu>
2019-12-02 15:32:04 -0500

    Updated publishing script.

--- publish.sh
+++ publish.sh
@@ -4,7 +4,7 @@
 #
 # This script will publish your local build of the site to the
 # webserver.  To use it, you must have 1) access to systems-v2, 2)
-# must have been added to the www-data group on systems-v2, and 3) an
+# membership in the www-data group on systems-v2, and 3) an
 # ~/.ssh/config entry for systems-v2 called "systems-v2".  If you
 # intend to run this script from outside the CS network, you must also
 # include an entry to use the department SSH gateway, such as the
@@ -21,11 +21,15 @@
 #    IdentityFile ~/.ssh/id_rsa
 #    ProxyCommand ssh browncs -W %h:%p
 #
+# Note:  If you have issues, make sure you can connect to pk-ssh.cs.brown.edu
+# manually.  Using this server requires you have a cached Kerberos ticket, which
+# may require you to ssh to ssh.cs.brown.edu manually first.
+#
 # Note: running this script will overwrite any changes or extra files
 # in the webserver's directory for the site without warning.
 
 set -euo pipefail
-BUNDLE=/usr/bin/bundle
+BUNDLE=$(which bundle)
 
 ${BUNDLE} exec jekyll clean
 ${BUNDLE} exec jekyll build
