74e1387d42d131e52d1fbaa30a20327b53bceade
Nicholas DeMarinis <ndemarinis@wpi.edu>
2019-12-13 22:54:02 -0500

    Fixed twitter link and publish.sh script.

--- _news/new_website.md
+++ _news/new_website.md
@@ -4,4 +4,4 @@ date: 2019-11-06 15:59:00-0400
 inline: true
 ---
 
-We have a brand new website and [Twitter account](https://twitter.com/brownsystems)!
+We have a brand new website and [Twitter account](https://twitter.com/systemsatbrown)!
--- publish.sh
+++ publish.sh
@@ -3,9 +3,9 @@
 # Silly direct publishing script
 #
 # This script will publish your local build of the site to the
-# webserver.  To use it, you must have 1) access to systems-v2, 2)
-# membership in the www-data group on systems-v2, and 3) an
-# ~/.ssh/config entry for systems-v2 called "systems-v2".  If you
+# webserver.  To use it, you must have 1) access to systems-v3, 2)
+# membership in the www-data group on systems-v3, and 3) an
+# ~/.ssh/config entry for systems-v3 called "systems-v3".  If you
 # intend to run this script from outside the CS network, you must also
 # include an entry to use the department SSH gateway, such as the
 # following:
@@ -16,7 +16,7 @@
 #    IdentityFile ~/.ssh/id_rsa
 #
 # Host systems-v2
-#    HostName systems-v2.cs.brown.edu
+#    HostName systems-v3.cs.brown.edu
 #    User <YOUR USER>
 #    IdentityFile ~/.ssh/id_rsa
 #    ProxyCommand ssh browncs -W %h:%p
@@ -29,10 +29,10 @@
 # in the webserver's directory for the site without warning.
 
 set -euo pipefail
-BUNDLE=$(which bundle)
+BUNDLE=/usr/bin/bundle
 
 ${BUNDLE} exec jekyll clean
 ${BUNDLE} exec jekyll build
 
-rsync -avz --update --delete --chmod=D775,F774 --progress _site/* systems-v2:/var/www/systems
+rsync -avz --update --delete --chmod=D775,F774 --progress _site/* systems-v3:/vol/web/html/systems
 
