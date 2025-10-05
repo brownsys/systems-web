05bf6cb05614348b51beffcf134e632861cb5054
mlazoy <mlazoy@icloud.com>
2025-10-05 16:45:33 -0400

    navbar template

--- assets/components/navbar.js
+++ assets/components/navbar.js
@@ -1,30 +1,32 @@
 class SiteHeader extends HTMLElement {
-    connectedCallback() {
-      this.innerHTML = `
-        <header class="site-header">
-          <div class="wrapper">
-            <nav class="site-nav">
-              <div class="site-title"><a href="../index.html">Systems@Brown</a></div>
-              <div class="trigger">
-                <a class="page-link" href="../index.html">Home</a>
-                <a class="page-link" href="../projects/index.html">Projects</a>
-                <a class="page-link" href="../publications/index.html">Publications</a>
-                <a class="page-link" href="../sysread/index.html">Sysread</a>
-              </div>
-            </nav>
-          </div>
-        </header>
-      `;
-  
-      // Optional: highlight the active page link
-      const current = window.location.pathname.split("/").pop();
-      this.querySelectorAll(".page-link").forEach(link => {
-        if (link.getAttribute("href").includes(current)) {
-          link.classList.add("active");
-        }
-      });
-    }
+  connectedCallback() {
+    const depth = window.location.pathname.split("/").filter(Boolean).length - 1;
+    const prefix = "../".repeat(depth);
+
+    this.innerHTML = `
+      <header class="site-header">
+        <div class="wrapper">
+          <nav class="site-nav">
+            <div class="site-title"><a href="${prefix}index.html">Systems@Brown</a></div>
+            <div class="trigger">
+              <a class="page-link" href="${prefix}index.html">Home</a>
+              <a class="page-link" href="${prefix}projects/index.html">Projects</a>
+              <a class="page-link" href="${prefix}publications/index.html">Publications</a>
+              <a class="page-link" href="${prefix}sysread/index.html">Sysread</a>
+            </div>
+          </nav>
+        </div>
+      </header>
+    `;
+
+    // Highlight active link
+    const current = window.location.pathname.split("/").pop();
+    this.querySelectorAll(".page-link").forEach(link => {
+      if (link.getAttribute("href").includes(current)) {
+        link.classList.add("active");
+      }
+    });
   }
-  
-  customElements.define("site-header", SiteHeader);
-  
+}
+
+customElements.define("site-header", SiteHeader);
