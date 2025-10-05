f12c01e268542da45c1bdeab8d7c1e2509f98299
mlazoy <mlazoy@icloud.com>
2025-10-05 17:49:30 -0400

    updated courses and 3 papers added

--- assets/components/navbar.js
+++ assets/components/navbar.js
@@ -1,7 +1,7 @@
 class SiteHeader extends HTMLElement {
   connectedCallback() {
-    const depth = window.location.pathname.split("/").filter(Boolean).length - 1;
-    const prefix = "../".repeat(depth);
+    const depth = Math.max(0, window.location.pathname.split("/").filter(Boolean).length - 1);
+    const prefix = "../".repeat(depth);    
 
     this.innerHTML = `
       <header class="site-header">
@@ -10,9 +10,11 @@ class SiteHeader extends HTMLElement {
             <div class="site-title"><a href="${prefix}index.html">Systems@Brown</a></div>
             <div class="trigger">
               <a class="page-link" href="${prefix}index.html">Home</a>
+              <a class="page-link" href="${prefix}index.html#people">People</a>
               <a class="page-link" href="${prefix}projects/index.html">Projects</a>
               <a class="page-link" href="${prefix}publications/index.html">Publications</a>
               <a class="page-link" href="${prefix}sysread/index.html">Sysread</a>
+              <a class="page-link" href="https://brown-systems-week.github.io">Week</a>
             </div>
           </nav>
         </div>
