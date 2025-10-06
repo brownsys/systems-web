d47e3b3446a980b25aa84920f4709e3777d26664
mlazoy <mlazoy@icloud.com>
2025-10-06 18:45:54 -0400

    static base path

--- assets/components/navbar.js
+++ assets/components/navbar.js
@@ -1,19 +1,24 @@
 class SiteHeader extends HTMLElement {
   connectedCallback() {
-    const depth = Math.max(0, window.location.pathname.split("/").filter(Boolean).length - 1);
-    const prefix = "../".repeat(depth);    
+    // Dynamically choose base URL depending on environment
+    const isLocalhost = window.location.hostname === "localhost";
+    const base = isLocalhost
+      ? "http://localhost:8000/"
+      : "https://atlas.cs.brown.edu/KtbxLzFvRgzPLQTVqWjTslGNMtkHCvzbDB/systems-at-brown/";
 
     this.innerHTML = `
       <header class="site-header">
         <div class="wrapper">
           <nav class="site-nav">
-            <div class="site-title"><a href="${prefix}index.html">Systems@Brown</a></div>
+            <div class="site-title">
+              <a href="${base}index.html">Systems@Brown</a>
+            </div>
             <div class="trigger">
-              <a class="page-link" href="${prefix}index.html">Home</a>
-              <a class="page-link" href="${prefix}index.html#people">People</a>
-              <a class="page-link" href="${prefix}projects/index.html">Projects</a>
-              <a class="page-link" href="${prefix}publications/index.html">Publications</a>
-              <a class="page-link" href="${prefix}sysread/index.html">Sysread</a>
+              <a class="page-link" href="${base}index.html">Home</a>
+              <a class="page-link" href="${base}index.html#people">People</a>
+              <a class="page-link" href="${base}projects/index.html">Projects</a>
+              <a class="page-link" href="${base}publications/index.html">Publications</a>
+              <a class="page-link" href="${base}sysread/index.html">Sysread</a>
               <a class="page-link" href="https://brown-systems-week.github.io">Week</a>
             </div>
           </nav>
@@ -21,7 +26,7 @@ class SiteHeader extends HTMLElement {
       </header>
     `;
 
-    // Highlight active link
+    // Highlight the active link
     const current = window.location.pathname.split("/").pop();
     this.querySelectorAll(".page-link").forEach(link => {
       if (link.getAttribute("href").includes(current)) {
@@ -32,3 +37,4 @@ class SiteHeader extends HTMLElement {
 }
 
 customElements.define("site-header", SiteHeader);
+
